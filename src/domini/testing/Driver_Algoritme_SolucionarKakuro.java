b53c3340d737c978d5ede13247d1b7d32632a7c6
PC casa <ceschen27@gmail.com>
2020-10-30 17:45:17 +0100

    Driver solucionar kakuro

--- src/domini/testing/Driver_Algoritme_SolucionarKakuro.java
+++ src/domini/testing/Driver_Algoritme_SolucionarKakuro.java
@@ -1,9 +1,22 @@
 package domini.testing;
 
+import domini.algoritme.Algoritme;
+import domini.tauler.TaulerEnunciat;
+import presistencia.Dades;
+
 public class Driver_Algoritme_SolucionarKakuro {
+    public static Algoritme algoritme = new Algoritme();
 
     public static void main(String[] args) {
-        System.out.println("Generador");
+
+        System.out.println("TAULER EXEMPLE: ");
+        String e2 = Dades.carregaArxiu("dades/exemple.txt");
+        TaulerEnunciat t = new TaulerEnunciat(e2);
+        t.print();
+
+        System.out.println("\n\nTAULER RESOLT: ");
+        algoritme.resoldreKakuro(t);
+        t.print();
 
     }
 }
--- src/domini/testing/Driver_Algoritme_generarKakuro.java
+++ src/domini/testing/Driver_Algoritme_generarKakuro.java
@@ -1,24 +1,13 @@
 package domini.testing;
 
-import domini.algoritme.Algoritme;
-import domini.tauler.TaulerEnunciat;
-import domini.tauler.casella.CasellaBlanca;
-import presistencia.Dades;
+
 
 public class Driver_Algoritme_generarKakuro {
-    public static Algoritme algoritme = new Algoritme();
 
     public static void main(String[] args) {
         System.out.println("TESTING GENERADOR DE KAKURO: ");
 
-        System.out.println("TAULER EXEMPLE: ");
-        String e2 = Dades.carregaArxiu("dades/exemple.txt");
-        TaulerEnunciat t = new TaulerEnunciat(e2);
-        t.print();
 
-        System.out.println("\n\nTAULER RESOLT: ");
-        algoritme.resoldreKakuro(t);
-        t.print();
     }
     
 }
