0ddad46dcc4fcd2377adfc0699668e53efe70fe2
marc.bofill.janer <marc.bofill.janer@est.fib.upc.edu>
2020-10-30 17:12:48 +0100

    Drivers: Caselles i Combinacions

---
+++ src/domini/testing/Driver_Combinacions.java
@@ -0,0 +1,18 @@
+package domini.testing;
+
+import domini.algoritme.Combinacions;
+import java.util.Arrays;
+
+public class Driver_Combinacions {
+    public static void main(String[] args) {
+
+        Combinacions combinacions = new Combinacions();
+
+        for (int suma : combinacions.getSum()) {
+            for (int blanca : combinacions.getBlanques(suma)) {
+                System.out.println("suma:" + suma + " blanques:" + blanca);
+                System.out.println(Arrays.deepToString(combinacions.getCombinacios(suma, blanca)));
+            }
+        }
+    }
+}
