2b135acdf276c5c5d4e842525abb77d1ae2890d0
marc.bofill.janer <marc.bofill.janer@est.fib.upc.edu>
2020-10-30 17:14:44 +0100

    Drivers: Caselles i Combinacions

--- src/domini/launcher/Main.java
+++ src/domini/launcher/Main.java
@@ -1,27 +1,8 @@
 package domini.launcher;
 
-import domini.algoritme.Combinacions;
-
-import java.util.Arrays;
-
 public class Main {
 
     public static void main(String[] args) {
         //Joc joc = new Joc();
-
-        Combinacions combinacions = new Combinacions();
-        printTotsElsCombinacions(combinacions);
-
-
-    }
-
-    //test perque veieu com funciona la clase Combinacions
-    public static void printTotsElsCombinacions(Combinacions combinacions) {
-        for (int suma : combinacions.getSum()) {
-            for (int blanca : combinacions.getBlanques(suma)) {
-                System.out.println("suma:" + suma + " blanques:" + blanca);
-                System.out.println(Arrays.deepToString(combinacions.getCombinacios(suma, blanca)));
-            }
-        }
     }
 }
