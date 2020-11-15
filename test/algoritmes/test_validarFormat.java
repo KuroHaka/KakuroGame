60ac78ba5fe5cc46f7def081fbcf3fda4c3b2236
xavier.girbau <xavier.girbau@estudiantat.upc.edu>
2020-11-15 18:32:00 +0100

    Test: Validar Format

--- test/algoritmes/test_validarFormat.java
+++ test/algoritmes/test_validarFormat.java
@@ -1,5 +1,26 @@
 package algoritmes;
 
+import domini.algoritme.Algoritme;
+import domini.tauler.TaulerEnunciat;
+import presistencia.Dades;
+
 public class test_validarFormat {
+    public static Algoritme algoritme = new Algoritme();
     
+    private static void test(String nom, boolean validesaReal) {
+        String arxiu = Dades.carregaArxiu("test/algoritmes/"+nom);
+        TaulerEnunciat enunciat = new TaulerEnunciat(arxiu);
+        boolean validesa = algoritme.validaFormat(enunciat);
+        assert validesa == validesaReal;
+        
+        if (validesa != validesaReal) System.out.println(nom + ": no ha passat el test.");
+    }
+    
+    public static void main(String[] args) {
+        System.out.println("TEST VALIDADOR DE FORMATS DE ENUNCIATS: ");
+        
+        test("validaFormat001.in", false);
+        ///// AFEGIR TESTOS /////
+        
+    }
 }
