ec77c77e59ae689f2ee6d3137acdac63794b353e
xavier.girbau <xavier.girbau@estudiantat.upc.edu>
2020-11-15 22:39:41 +0100

    Test: Validar Solucio

--- test/algoritmes/test_validarFormat.java
+++ test/algoritmes/test_validarFormat.java
@@ -10,16 +10,21 @@ public class test_validarFormat {
     private static void test(String nom, boolean validesaReal) {
         String arxiu = Dades.carregaArxiu("test/algoritmes/"+nom);
         TaulerEnunciat enunciat = new TaulerEnunciat(arxiu);
-        boolean validesa = algoritme.validaFormat(enunciat);
-        assert validesa == validesaReal;
         
+        boolean validesa = algoritme.validaFormat(enunciat);
+        
+        assert validesa == validesaReal;
         if (validesa != validesaReal) System.out.println(nom + ": no ha passat el test.");
     }
     
     public static void main(String[] args) {
         System.out.println("TEST VALIDADOR DE FORMATS DE ENUNCIATS: ");
         
+        System.out.println("Test 001...");
         test("validaFormat001.in", false);
+        System.out.println("Test 002...");
+        test("validaFormat002.in", true);
+        System.out.println("Test 003...");
         ///// AFEGIR TESTOS /////
         
     }
--- test/algoritmes/test_validarSolucio.java
+++ test/algoritmes/test_validarSolucio.java
@@ -1,5 +1,31 @@
 package algoritmes;
 
+import domini.algoritme.Algoritme;
+import domini.tauler.TaulerComencat;
+import presistencia.Dades;
+
 public class test_validarSolucio {
+    // Donat una solució, s'ha de veure si és correcte
+    public static Algoritme algoritme = new Algoritme();
     
+    public static void test(String nom, boolean validesaReal) {
+        String arxiu = Dades.carregaArxiu("test/algoritmes/"+nom);
+        TaulerComencat tauler = new TaulerComencat(arxiu);
+        
+        boolean validesa = algoritme.validaSolucio(tauler);
+        
+        assert validesa == validesaReal;
+        if (validesa != validesaReal) System.out.println(nom + ": no ha passat el test.");
+    }
+    
+    public static void main(String[] args) {
+        System.out.println("TEST VALIDADOR DE SOLUCIONS DE KAKUROS: ");
+        
+        System.out.println("Test 001...");
+        test("validaSolucio001.in", true);
+        System.out.println("Test 002...");
+        test("validaSolucio002.in", false);
+        System.out.println("Test 003...");
+        ///// AFEGIR TESTOS /////
+    }
 }
