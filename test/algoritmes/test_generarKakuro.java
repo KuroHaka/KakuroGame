dc6494fb0139331c23f3ea30265ec20eadea7d2a
xavier.girbau <xavier.girbau@estudiantat.upc.edu>
2020-11-17 21:14:09 +0100

    Test: Generar Kakuro

--- test/algoritmes/test_generarKakuro.java
+++ test/algoritmes/test_generarKakuro.java
@@ -1,5 +1,33 @@
 package algoritmes;
 
+import domini.algoritme.Algoritme;
+import domini.tauler.TaulerEnunciat;
+
 public class test_generarKakuro {
+    public static Algoritme algoritme = new Algoritme();
     
+    private static void test(int x, int y, boolean validesaReal) {
+        TaulerEnunciat enunciat = algoritme.generarKakuroSimple(x,y,null);
+        //enunciat.print();
+        boolean validesa = algoritme.validaFormat(enunciat);
+        
+        assert validesa == validesaReal;
+        if (validesa != validesaReal) {
+            System.out.println(x+","+y+ ": no ha passat el test.");
+            enunciat.print();
+        }
+    }
+    
+    public static void main(String[] args) {
+        System.out.println("TEST GENERADOR DE KAKUROS: ");
+        
+        for (int k = 0; k < 10; k++) {
+            for (int i = 1; i <= 20; i++) {
+                for (int j = 1; j <= 20; j++) {
+                    System.out.println("Test " + i + "," + j + ":");
+                    test(i, j, true);
+                }
+            }
+        }
+    }
 }
