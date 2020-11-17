a925467555ef0556ea6f4163071727f7a6299734
marc.bofill.janer <marc.bofill.janer@est.fib.upc.edu>
2020-11-17 20:31:03 +0100

    Estructura entrega. Script entrega automatica

---
+++ generaEntrega.sh
@@ -0,0 +1,21 @@
+# FONTS
+rm -r ENTREGA/FONTS/*
+cp -r src/* ENTREGA/FONTS/
+
+
+# EXE : executables (drivers) de les classes + jocs de prova
+
+# Classe Algoritme
+cp -r src/domini/algoritme/* ENTREGA/EXE/algoritme
+
+
+
+#rm -r ENTREGA/FONTS/
+# javac arxiu.java
+# jar cvf arxiu.jar
+# to execute: java -jar arxiu.jar arxiu.class
+
+
+#ZIP de Entrega
+#S'ha de fer el ZIP de l'entrega. CARPETA ENTREGA
+tar -czvf 1aENTREGA_9.2.tar.gz ENTREGA
