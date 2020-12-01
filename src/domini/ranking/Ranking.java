0ae32797d05988f4c256d25e1ec88f6707515eea
xavier.girbau <xavier.girbau@estudiantat.upc.edu>
2020-12-01 20:32:13 +0100

    Ranking començat

--- src/domini/ranking/Ranking.java
+++ src/domini/ranking/Ranking.java
@@ -1,5 +1,44 @@
 package domini.ranking;
 
+import domini.tauler.TaulerEnunciat;
+import domini.usuari.Usuari;
+
+import java.util.ArrayList;
+import java.util.Map;
+
 public class Ranking {
     //TODO
+    private ArrayList<RankingEntry> R;
+    private int size;
+    
+    ///// Constructora /////
+    public Ranking() {
+        this.R = new ArrayList<RankingEntry>();
+    }
+    
+    public void afegirEntry(int id, int temps) {
+        boolean exist = false;
+        for (int i = 0; i < size; i++) {
+            if (R.get(i).getId() == id) {
+                if (R.get(i).getTempsRecord() > temps) {
+                    R.get(i).modificarTemps(temps);
+                }
+                exist = true;
+                break;
+            }
+        }
+        if (!exist) {
+            R.add(new RankingEntry(id, temps));
+            size++;
+        }
+    }
+    
+    ///// Consultores /////
+    
+    public void print() {
+        System.out.println("Rànkings: ");
+        for (int i = 0; i < size; i++) {
+            R.get(i).printEntry();
+        }
+    }
 }
--- src/domini/ranking/RankingEntry.java
+++ src/domini/ranking/RankingEntry.java
@@ -1,5 +1,35 @@
 package domini.ranking;
 
-public class RankingEntry {
+public class RankingEntry extends Ranking{
     //TODO
+    private int id;
+    private int tempsRecord;
+    
+    ///// Constructora /////
+    protected RankingEntry(int id, int tempsRecord) {
+        this.id = id;
+        this.tempsRecord = tempsRecord;
+    }
+    
+    ///// Consultores /////
+    
+    protected int getId() {
+        return id;
+    }
+    
+    protected int getTempsRecord() {
+        return tempsRecord;
+    }
+    
+    public void print() {
+        System.out.println("ID = " + id);
+        System.out.println("Durada = " + tempsRecord);
+    }
+    
+    ///// Modificadores /////
+    
+    protected void modificarTemps(int temps) {
+        this.tempsRecord = temps;
+    }
+    
 }
